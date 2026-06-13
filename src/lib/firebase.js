fb3dd44c263d81a4b88986304773c745b7ccb7f8
strackerfoxx <hokaygames358@gmail.com>
2026-06-12 22:08:18 -0600

    firebase config variables protected (changed to env)

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ9K0BprS8JWKqfiILr_f76rwHeW4371I",
-  authDomain: "aera-3c7a2.firebaseapp.com",
-  projectId: "aera-3c7a2",
-  storageBucket: "aera-3c7a2.firebasestorage.app",
-  messagingSenderId: "486481448152",
-  appId: "1:486481448152:web:3c0ad6ea0dff5ebd58dbad",
-  measurementId: "G-J8X1VZT6PE"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
